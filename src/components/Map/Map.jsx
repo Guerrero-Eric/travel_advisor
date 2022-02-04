a59e45834f6a071267a1b19558c7c4cff2716fa6
eg661 <eg6616@gmail.com>
2022-02-03 18:18:33 -0800

    List menu

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab";
+
+import useStyles from "./styles";
+
+const Map = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(min-width: 600px)");
+  {
+    /*False if larger than 600px*/
+  }
+  const coordinates = { lat: 0, lng: 0 };
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyBq6uo2-fCySz8dC68MuPQiFz3dC7QxQyQ' }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        onChange={''}
+        onChildClick={''}
+      ></GoogleMapReact>
+    </div>
+  );
+};
+
+export default Map;
