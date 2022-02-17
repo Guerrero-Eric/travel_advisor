6912b096d7ed4082972a31cada06ee529e629464
eg661 <eg6616@gmail.com>
2022-02-16 20:38:56 -0800

    added icons on map

--- src/App.js
+++ src/App.js
@@ -37,13 +37,14 @@ const App = () => {
       <Grid container spacing={3} style={{ width: "100%" }}>
         <Grid item xs={12} md={4}>
           {/*Full width on mobile or 4 spaces on mid to large devices.*/}
-          <List places = {places}/>
+          <List places={places} />
         </Grid>
         <Grid item xs={12} md={8}>
           <Map
             setCoordinates={setCoordinates}
             setBounds={setBounds}
             coordinates={coordinates}
+            places={places}
           />
         </Grid>
       </Grid>
